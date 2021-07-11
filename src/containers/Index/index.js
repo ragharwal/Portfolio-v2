7530db26845ea42b3f8bdbf8a00ab5a436b3a204
Sanyam Mehendiratta <sanyammehendiratta2000@gmail.com>
2021-07-11 18:32:10 +0530

    changes

--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -24,7 +24,10 @@ const data = {
           list: [
             {
               Component: InfoBadge,
-              props: { badgeText: "Languages", badgeSubtext: "C/C++ , Java, Javascript and Python" },
+              props: {
+                badgeText: "Languages",
+                badgeSubtext: "C/C++ , Java, Javascript and Python",
+              },
             },
             {
               Component: InfoBadge,
@@ -163,12 +166,13 @@ const data = {
                 to: "Present",
                 from: "July, 2019",
                 role: "B.Tech (Hons) Computer Science and Engineering - Big Data",
-                companyName: "University of Petroleum and Energy Studies, Dehradun",
+                companyName:
+                  "University of Petroleum and Energy Studies, Dehradun",
                 description: [
                   "GPA: 8.81/10",
                   "Lead Student Developer at Open Community",
                   "Technical Head at ThinkBig Data Community",
-                  "Core Committee Member at National Service Scheme"
+                  "Core Committee Member at National Service Scheme",
                 ],
               },
             },
@@ -180,9 +184,9 @@ const data = {
                 role: "Senior Secondary School",
                 companyName: "Asha Modern School, Saharanpur",
                 description: [
-                  "Percentage: 91.1%", 
+                  "Percentage: 91.1%",
                   "Member at Discipline Committee",
-                  "Prefect at Marsahal Neil House"
+                  "Prefect at Marsahal Neil House",
                 ],
               },
             },
@@ -196,7 +200,7 @@ const data = {
                 description: [
                   "CGPA: 7.8/10",
                   "Member at Prithvi Mitra Society",
-                  "Assistant Prefect at Marsahal Neil House"
+                  "Assistant Prefect at Marsahal Neil House",
                 ],
               },
             },
